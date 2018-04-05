b10af508d41dcef6c22f7f06336b715773ada0dd
MiterX <you@example.com>
2018-04-05 06:59:04 +0200

    make it better

---
+++ Key_secret.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'ePjadq1jg0svapgQQ7JKFeraJ',
+  consumer_secret:      'tkfRoylA9WjQvDFJliwulIU00yNt3ZvjvhQ6jBF3hjMIl9qIyl',
+  access_token:         '434349520-Rkrp2cmxXfMszI0mdwb4WO8AhydVIHe9bcFtD9ob',
+  access_token_secret:  'CjFpAfHx4YqcRYj3MhOxMusunhQWhoJuf7KpUcjERRoba',
+  // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
